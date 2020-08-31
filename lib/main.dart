3c0cf65a8175dc80c93c3f6c7f486cee7144e005
Crewzul12 <zulhresident12@gmail.com>
2020-08-31 21:25:56 +0800

    jwt with expiration feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,161 @@
-// import 'package:HiroDeli/screens/MyAdress.dart';
+import 'package:HiroDeli/screens/MyAdress.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_svg/flutter_svg.dart';
-// import 'package:HiroDeli/screens/Loading.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:HiroDeli/screens/Login.dart';
 import 'package:HiroDeli/screens/TestJwtPage.dart';
-// import 'package:HiroDeli/screens/Signup.dart';
-// import 'package:HiroDeli/screens/ForgotPassword.dart';
-// import 'package:HiroDeli/screens/SignupName.dart';
-// import 'package:HiroDeli/screens/SignupPhoneNum.dart';
-// import 'package:HiroDeli/screens/ResetPasswordSuccess.dart';
-// import 'package:HiroDeli/screens/Mainpage.dart';
-// import 'package:HiroDeli/screens/Cart.dart';
-// import 'package:HiroDeli/screens/SearchStore.dart';
-// import 'package:HiroDeli/screens/Profile.dart';
-// import 'package:HiroDeli/screens/GeneralInformation.dart';
-// import 'package:HiroDeli/screens/MyOrders.dart';
-// import 'package:HiroDeli/screens/PaymentMethod.dart';
-// import 'package:HiroDeli/screens/Settings.dart';
-// import 'package:HiroDeli/screens/Refund.dart';
-// import 'package:HiroDeli/screens/ReportIssue.dart';
-// import 'package:HiroDeli/screens/QrCodeScanner.dart';
-// import 'package:HiroDeli/screens/Location.dart';
-// import 'dart:convert'
-//     show json, base64, ascii; // Convert Json string into Json object
+import 'package:HiroDeli/screens/Signup.dart';
+import 'package:HiroDeli/screens/ForgotPassword.dart';
+import 'package:HiroDeli/screens/SignupName.dart';
+import 'package:HiroDeli/screens/SignupPhoneNum.dart';
+import 'package:HiroDeli/screens/ResetPasswordSuccess.dart';
+import 'package:HiroDeli/screens/Mainpage.dart';
+import 'package:HiroDeli/screens/Cart.dart';
+import 'package:HiroDeli/screens/SearchStore.dart';
+import 'package:HiroDeli/screens/Profile.dart';
+import 'package:HiroDeli/screens/GeneralInformation.dart';
+import 'package:HiroDeli/screens/MyOrders.dart';
+import 'package:HiroDeli/screens/PaymentMethod.dart';
+import 'package:HiroDeli/screens/Settings.dart';
+import 'package:HiroDeli/screens/Refund.dart';
+import 'package:HiroDeli/screens/ReportIssue.dart';
+import 'package:HiroDeli/screens/QrCodeScanner.dart';
+import 'package:HiroDeli/screens/Location.dart';
+import 'dart:convert'
+    show json, base64, ascii; // Convert Json string into Json object
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   Future<String> get jwtOrEmpty async {
-    var jwt = await storage.read(key: "jwt"); // Check if there is jwtoken in persistent storage
+    var jwt = await storage.read(key: "jwt");
     if (jwt == null) return "";
     return jwt;
   }
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xddd60606),
+        body: Container(
+          child: Stack(
+            children: <Widget>[
+              Center(
+                child: SvgPicture.asset(
+                  'assets/hirodeli-logo.svg',
+                  width: 290,
+                  height: 170,
+                ),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child: FlatButton(
+                          color: Color(0xffffffff),
+                          padding: EdgeInsets.all(20),
+                          onPressed: () {
+                            Navigator.of(context).pushNamed('/login');
+                          },
+                          child: Text(
+                            'LOG IN',
+                            style: TextStyle(
+                              color: Colors.cyanAccent[700],
+                            ),
+                          ),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: OutlineButton(
+                          padding: EdgeInsets.all(20),
+                          onPressed: () {
+                            Navigator.of(context).pushNamed('/signup');
+                          },
+                          child: Text(
+                            'SIGN UP',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          borderSide: BorderSide(
+                            color: Colors.redAccent, //Color of the border
+                            style: BorderStyle.solid, //Style of the border
+                            width: 0.5, //width of the border
+                          ),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class MyApp extends StatelessWidget {
+  Future<String> get jwtOrEmpty async {
+    var jwt = await storage.read(
+        key: "jwt"); // Check if there is jwtoken in persistent storage
+    if (jwt == null) return ""; // If jwt is null, return null
+    return jwt;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
+      routes: <String, WidgetBuilder>{
+        // MaterialApp routes
+        '/home': (BuildContext context) => Home(), // Home page route
+        '/login': (BuildContext context) => Login(), // Login page route
+        '/signup': (BuildContext context) => Signup(), // Signup page route
+        '/forgotPassword': (BuildContext context) =>
+            ForgotPassword(), // ForgotPassword page route
+        '/signupName': (BuildContext context) =>
+            SignupName(), // SignupName page route
+        '/signupPhoneNum': (BuildContext context) =>
+            SignupPhoneNum(), // SignupPhoneNum page route
+        '/resetPasswordSuccess': (BuildContext context) =>
+            ResetPasswordSuccess(), // ResetPasswordSuccess page route
+        '/mainPage': (BuildContext context) =>
+            Mainpage(), // Mainpage page route
+        '/cart': (BuildContext context) => Cart(), // Cart page route
+        '/searchStore': (BuildContext context) =>
+            SearchStore(), // SearchStore page route
+        '/profile': (BuildContext context) => Profile(), // Profile page route
+        '/generalInfo': (BuildContext context) =>
+            GeneralInformation(), // GeneralInformation page route
+        '/myAddress': (BuildContext context) =>
+            MyAdress(), // MyAdress page route
+        '/myOrders': (BuildContext context) =>
+            MyOrders(), // MyOrders page route
+        '/paymentMethod': (BuildContext context) =>
+            PaymentMethod(), // PaymentMethod page route
+        '/settings': (BuildContext context) =>
+            Settings(), // Settings page route
+        '/refund': (BuildContext context) => Refund(), // Refund page route
+        '/reportIssue': (BuildContext context) =>
+            ReportIssue(), // ReportIssue page route
+        '/qrCodeScanner': (BuildContext context) =>
+            QrCodeScanner(), // QrCodeScanner page route
+        '/location': (BuildContext context) =>
+            Location(), // Location page route
+      },
       title: 'Authentication Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -47,125 +165,28 @@ class MyApp extends StatelessWidget {
           builder: (context, snapshot) {
             if (!snapshot.hasData) return CircularProgressIndicator();
             if (snapshot.data != "") {
-              var str = snapshot.data;
-              if (str == null) {
-                return Login();
+              var str = snapshot.data; // Receive jwt from FutureBuilder
+              var jwt = str.split("."); // Split jwt into 3 different strings
+
+              if (jwt.length != 3) {
+                // Check if jwt has 3 different strings which is 3 dots, if it has it, it will return TestJwtPage, otherwise return Home page
+                return Home();
               } else {
-                return TestJwtPage(str);
+                var payload = json.decode(ascii.decode(base64.decode(
+                    base64.normalize(
+                        jwt[1])))); // JWT payload receive from FutureBuilder
+                if (DateTime.fromMillisecondsSinceEpoch(payload["exp"] * 1000)
+                    .isAfter(DateTime.now())) {
+                  // Check if expiration jwt date is after current date, if it is, it will return TestJwtPage which means not expired yet, otherwise it will return Home page which is expired
+                  return TestJwtPage(str, payload);
+                } else {
+                  return Home();
+                }
               }
             } else {
-              return Login();
+              return Home();
             }
           }),
     );
   }
 }
-
-// void main() => runApp(MaterialApp(
-//       initialRoute: '/home',
-//       routes: <String, WidgetBuilder>{
-//         '/': (BuildContext context) => Loading(),
-//         '/home': (BuildContext context) => Home(),
-//         '/login': (BuildContext context) => Login(),
-//         '/signup': (BuildContext context) => Signup(),
-//         '/forgotPassword': (BuildContext context) => ForgotPassword(),
-//         '/signupName': (BuildContext context) => SignupName(),
-//         '/signupPhoneNum': (BuildContext context) => SignupPhoneNum(),
-//         '/resetPasswordSuccess': (BuildContext context) =>
-//             ResetPasswordSuccess(),
-//         '/mainPage': (BuildContext context) => Mainpage(),
-//         '/cart': (BuildContext context) => Cart(),
-//         '/searchStore': (BuildContext context) => SearchStore(),
-//         '/profile': (BuildContext context) => Profile(),
-//         '/generalInfo': (BuildContext context) => GeneralInformation(),
-//         '/myAddress': (BuildContext context) => MyAdress(),
-//         '/myOrders': (BuildContext context) => MyOrders(),
-//         '/paymentMethod': (BuildContext context) => PaymentMethod(),
-//         '/settings': (BuildContext context) => Settings(),
-//         '/refund': (BuildContext context) => Refund(),
-//         '/reportIssue': (BuildContext context) => ReportIssue(),
-//         '/qrCodeScanner': (BuildContext context) => QrCodeScanner(),
-//         '/location': (BuildContext context) => Location(),
-//       },
-//     ));
-
-// class Home extends StatefulWidget {
-//   @override
-//   _HomeState createState() => _HomeState();
-// }
-
-// class _HomeState extends State<Home> {
-//   Future<String> get jwtOrEmpty async {
-//     var jwt = await storage.read(key: "jwt");
-//     if (jwt == null) return "";
-//     return jwt;
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         backgroundColor: Color(0xddd60606),
-//         body: Container(
-//           child: Stack(
-//             children: <Widget>[
-//               Center(
-//                 child: SvgPicture.asset(
-//                   'assets/hirodeli-logo.svg',
-//                   width: 290,
-//                   height: 170,
-//                 ),
-//               ),
-//               Column(
-//                 mainAxisAlignment: MainAxisAlignment.end,
-//                 children: <Widget>[
-//                   Row(
-//                     children: <Widget>[
-//                       Expanded(
-//                         flex: 1,
-//                         child: FlatButton(
-//                           color: Color(0xffffffff),
-//                           padding: EdgeInsets.all(20),
-//                           onPressed: () {
-//                             Navigator.of(context).pushNamed('/login');
-//                           },
-//                           child: Text(
-//                             'LOG IN',
-//                             style: TextStyle(
-//                               color: Colors.cyanAccent[700],
-//                             ),
-//                           ),
-//                           shape: RoundedRectangleBorder(
-//                               borderRadius: BorderRadius.zero),
-//                         ),
-//                       ),
-//                       Expanded(
-//                         flex: 1,
-//                         child: OutlineButton(
-//                           padding: EdgeInsets.all(20),
-//                           onPressed: () {
-//                             Navigator.of(context).pushNamed('/signup');
-//                           },
-//                           child: Text(
-//                             'SIGN UP',
-//                             style: TextStyle(
-//                               color: Colors.white,
-//                             ),
-//                           ),
-//                           borderSide: BorderSide(
-//                             color: Colors.redAccent, //Color of the border
-//                             style: BorderStyle.solid, //Style of the border
-//                             width: 0.5, //width of the border
-//                           ),
-//                           shape: RoundedRectangleBorder(
-//                               borderRadius: BorderRadius.zero),
-//                         ),
-//                       ),
-//                     ],
-//                   )
-//                 ],
-//               ),
-//             ],
-//           ),
-//         ));
-//   }
-// }
--- lib/screens/TestJwtPage.dart
+++ lib/screens/TestJwtPage.dart
@@ -1,39 +1,62 @@
 import 'package:flutter/material.dart';
-// import 'dart:convert'
-//     show json, base64, ascii; // Convert Json string into Json object
-import 'package:http/http.dart'
-    as http; // Http client package for request and response
+import 'dart:convert'
+    show json, base64, ascii; // Convert Json string into Json object
+import 'package:flutter_secure_storage/flutter_secure_storage.dart'; // Persistent storage for JWToken
 
 class TestJwtPage extends StatelessWidget {
-  TestJwtPage(this.jwt);
+  TestJwtPage(
+      this.jwt, this.payload); // Receive jwt from FutureBuilder snapshot.data
 
-  factory TestJwtPage.fromBase64(String jwt) => TestJwtPage(jwt);
-
-  final String jwt;
-
+  factory TestJwtPage.fromBase64(String jwt) => TestJwtPage(
+      jwt,
+      json.decode(ascii.decode(base64.decode(base64.normalize(jwt.split(".")[
+          1]))))); // Split 3 dots from response body into different string and normalize
+  // Normalize will do the following:
+  // Unescape any %-escapes.
+  // Only allow valid characters (A-Z, a-z, 0-9, / and +).
+  // Normalize a _ or - character to / or +.
+  // Validate that existing padding (trailing = characters) is correct.
+  // If no padding exists, add correct padding if necessary and possible.
+  // Validate that the length is correct (a multiple of four).
+  final String jwt; // Jwt access token
+  final Map<String, dynamic> payload; // JWT payload from response body into Map structure
+  final storage =
+      FlutterSecureStorage(); // Flutter secure storage instance for persistent storage
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(title: Text("Secret Data Screen")),
         body: Center(
-          child: FutureBuilder(
-              future: http.read(
-                  'https://jwt-test-login.herokuapp.com/api/hello-view/',
-                  headers: {
-                    "Authorization": "JWT " + jwt,
-                    "X-Hiro-Deli-Api-Auth-X":
-                        "psXUoVRx.SDlmMEPPjLqe8vMgodzQKwYwuCaOrVsE"
-                  }),
-              builder: (context, snapshot) => snapshot.hasData
-                  ? Column(
-                      children: <Widget>[
-                        Text("Hirodeli Jwtoken test page"),
-                        Text(snapshot.data),
-                        Text(jwt)
-                      ],
-                    )
-                  : snapshot.hasError
-                      ? Text("An error occurred")
-                      : CircularProgressIndicator()),
-        ),
+            child: Column(
+          children: <Widget>[
+            Text(jwt),
+            Text(payload['token_type'].toString()), // Display token type
+            Text(payload['exp'].toString()), // Display expiration time and date in Epoch unit time
+            Text(payload['jti'].toString()),
+            Text(payload['user_id'].toString()), // Display user id
+            RaisedButton(
+              onPressed: () async {
+                await storage.delete(
+                    key: 'jwt'); // Delete persistent storage key named "jwt"
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/home', (Route<dynamic> route) => false);
+              },
+              textColor: Colors.white,
+              padding: const EdgeInsets.all(0.0),
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: <Color>[
+                      Color(0xFF0D47A1),
+                      Color(0xFF1976D2),
+                      Color(0xFF42A5F5),
+                    ],
+                  ),
+                ),
+                padding: const EdgeInsets.all(10.0),
+                child: const Text('LOG OUT', style: TextStyle(fontSize: 20)),
+              ),
+            ),
+          ],
+        )),
       );
 }
