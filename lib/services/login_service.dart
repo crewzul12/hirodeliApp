8df4f34cb853de72329a8b24b238e1f0c994ee6a
Crewzul12 <zulhresident12@gmail.com>
2020-09-08 16:58:29 +0800

    latest changes

--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -5,10 +5,11 @@ class LoginService {
   JwToken token; // JwToken instance to store token
   Future<JwToken> loginUser(String email, String password) async {
     // Future for loginUser return a specified value
+    
     var res = await http.post(
         'https://jwt-test-login.herokuapp.com/api/login/', // POST request to retrieve token by sending email and password at body of the request
         headers: {
-          "X-Hiro-Deli-Api-Auth-X": "dUzhUW49.mzskx1G7fxnJTSgkVVtsgj4vpwcDf11v"
+          "X-Hiro-Deli-Api-Auth-X": "$apiKey"
         },
         body: {
           "email": email,
